eba697d6b9a275956d87eeb052f6288cd08811b6
Beau Barker <beauinmelbourne@gmail.com>
2014-11-05 10:46:32 +1100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from distutils.core import setup
 
 setup(
     name='rpcserver',
-    version='1.0',
-    description='JSON-RPC 2.0 server library',
+    version='0.01',
+    description='JSON-RPC 2.0 server library.',
     author='Beau Barker',
     author_email='beauinmelbourne@gmail.com',
     url='https://bitbucket.org/beau-barker/rpcserver',
