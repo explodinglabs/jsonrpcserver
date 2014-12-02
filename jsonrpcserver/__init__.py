9a11826cdfa205c82c936c2f3bdd2292a48026b9
Beau Barker <beauinmelbourne@gmail.com>
2014-12-03 09:47:49 +1100

    Manual merge of MANIFEST.in

--- jsonrpcserver/__init__.py
+++ jsonrpcserver/__init__.py
@@ -1,10 +1,15 @@
-"""jsonrpcserver"""
+"""__init__.py"""
+
+import logging
 
 from flask import Blueprint
 
-from . import exceptions
-from .dispatch import dispatch
+logger = logging.getLogger('jsonrpcserver')
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.WARNING)
 
 bp = Blueprint('bp', __name__)
 
-from . import blueprint
+from jsonrpcserver import exceptions
+from jsonrpcserver import blueprint
+from jsonrpcserver.dispatch import dispatch
