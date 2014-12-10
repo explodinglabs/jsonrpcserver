197378bd0ada03794c1ba1c5ee1268e9e4c0255d
Beau Barker <beauinmelbourne@gmail.com>
2014-12-10 16:30:34 +1100

    Major rebuild of the exceptions. The base exception class is now named "JsonRpcServerError" (was previously RPCHandlerException). Exceptions now require more information to be given when calling them.

--- jsonrpcserver/exceptions.py
+++ jsonrpcserver/exceptions.py
@@ -5,57 +5,102 @@ See http://www.jsonrpc.org/specification#error_object"""
 
 import json
 
-from jsonrpcserver import rpc
+from jsonrpcserver import rpc, status
 
-class RPCHandlerException(Exception):
-    """Base class for the other rpchandler exceptions"""
 
-    def __init__(self, code, message, **kwargs):
-        super().__init__(message)
-        self.code = code
-        self.message = message
-        self.data = None
-        self.request_id = None
 
-        if 'data' in kwargs:
-            self.data = kwargs['data']
+class JsonRpcServerError(Exception):
+    """Base class for the other exceptions"""
 
-        if 'request_id' in kwargs:
-            self.request_id = kwargs['request_id']
+    def __init__(
+            self, http_status_code, jsonrpc_status_code, text, data=None, \
+            request_id=None):
+
+        super().__init__(text)
+        self.http_status_code = http_status_code
+        self.jsonrpc_status_code = jsonrpc_status_code
+        self.text = text
+        self.data = data
+        self.request_id = request_id
 
     def __str__(self):
         """Returns the error in a in JSON-RPC format response string"""
 
         return json.dumps(
-            rpc.error(self.request_id, self.code, self.message, self.data), \
-            sort_keys=False)
+            rpc.error(self.request_id, self.jsonrpc_status_code, self.text, \
+                self.data), sort_keys=False)
 
-class ParseError(RPCHandlerException):
-    """Could not parse the request string. Invalid JSON."""
+class ParseError(JsonRpcServerError):
+    """From the specs: 'Invalid JSON was received by the server. An error
+    occurred on the server while parsing the JSON text.'
+
+    ParseError is raised when the request is jumbled and cannot be parsed.
+
+    No arguments should be passed - there's no need for any further explanation,
+    and we can't give a request_id back, because the request was simply not
+    parsed."""
 
     def __init__(self):
-        super().__init__(-32700, 'Parse error')
+        super().__init__(
+            status.HTTP_400_BAD_REQUEST, status.JSONRPC_PARSE_ERROR_CODE, \
+            status.JSONRPC_PARSE_ERROR_TEXT)
 
-class InvalidRequest(RPCHandlerException):
-    """The JSON sent didn't validate against the JSON-RPC request schema."""
+class InvalidRequest(JsonRpcServerError):
+    """From the specs: 'The JSON sent is not a valid Request object.'
 
-    def __init__(self, **kwargs):
-        super().__init__(-32600, 'Invalid request', **kwargs)
+    InvalidRequest is raised in two situations:
 
-class MethodNotFound(RPCHandlerException):
-    """The method does not exist/is not available."""
+    1. When the request doesn't validate against json-rpc schema. In this case
+    it gives the reason as a string, chosen by the jsonschema package as the
+    best match.
 
-    def __init__(self, **kwargs):
-        super().__init__(-32601, 'Method not found', **kwargs)
+    2. When flask gets a client error such as 404. In this case the reason is
+    given as the HTTP status code name, such as "File not found".
 
-class InvalidParams(RPCHandlerException):
-    """Invalid method parameter(s)."""
+    string @reason: The reason the request is invalid, placed in the data field.
+    """
 
-    def __init__(self, message='Invalid params', **kwargs):
-        super().__init__(-32602, message, **kwargs)
+    def __init__(self, reason, request_id=None):
+        super().__init__(
+            status.HTTP_400_BAD_REQUEST, status.JSONRPC_INVALID_REQUEST_CODE, \
+            status.JSONRPC_INVALID_REQUEST_TEXT, reason, request_id)
 
-class InternalError(RPCHandlerException):
-    """Internal error."""
+class MethodNotFound(JsonRpcServerError):
+    """From the specs: 'The method does not exist / is not available.'
 
-    def __init__(self, message='Internal error', **kwargs):
-        super().__init__(-1, message, **kwargs)
+    MethodNotFound is raised when the message was parsed OK but the requested
+    method doesn't exist.
+
+    string @method_name: To be returned in the data field."""
+
+    def __init__(self, method_name, request_id=None):
+        super().__init__(
+            status.HTTP_400_BAD_REQUEST, status.JSONRPC_METHOD_NOT_FOUND_CODE, \
+            status.JSONRPC_METHOD_NOT_FOUND_TEXT, method_name, request_id)
+
+class InvalidParams(JsonRpcServerError):
+    """From the specs: 'Invalid method parameter(s).'
+
+    InvalidParams is raised by dispatch() method if a TypeError is caught when
+    trying to dispatch to the requested method.
+
+    It should also be raised by the application implementation itself, if params
+    are required but were not given or not valid.
+
+    The "params" param should be either a list of the invalid param names, or a
+    dict with further information about each."""
+
+    def __init__(self, params, request_id=None):
+        super().__init__(
+            status.HTTP_400_BAD_REQUEST, status.JSONRPC_INVALID_PARAMS_CODE, \
+            status.JSONRPC_INVALID_PARAMS_TEXT, params, request_id)
+
+class ServerError(JsonRpcServerError):
+    """From the specs: 'Internal JSON-RPC error.'
+
+    ServerError is a generic response: """
+
+    def __init__(self, data=None, request_id=None):
+        super().__init__(
+            status.HTTP_500_INTERNAL_ERROR, status.JSONRPC_SERVER_ERROR_CODE, \
+            status.JSONRPC_SERVER_ERROR_TEXT, data, request_id)
