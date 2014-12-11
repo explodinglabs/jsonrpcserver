01481e2f4c6817b02badd70c2cdc7f5e817f2c99
Beau Barker <beauinmelbourne@gmail.com>
2014-12-11 14:16:04 +1100

    Updated readme

--- run.py
+++ run.py
@@ -3,25 +3,29 @@
 An example app, demonstrating how to use the library.
 """
 
-import sys
 from flask import Flask
 from jsonrpcserver import bp, dispatch, exceptions
 
-# Create flask app and register the blueprint.
+
+# Create a Flask app and register the blueprint.
 app = Flask(__name__)
 app.register_blueprint(bp)
 
-# Make a route for client access, and dispatch.
+# Add a route to pass requests on to the handling methods.
 @app.route('/', methods=['POST'])
 def index():
-    return dispatch(sys.modules[__name__])
+    return dispatch(MyHandlers)
 
-# Write methods to carry out the requests.
-def add(num1, num2='Not a number'):
-    try:
-        return num1 + num2
-    except TypeError as e:
-        raise exceptions.InvalidParams(str(e))
+
+# Now write the methods that carry out the requests.
+class MyHandlers:
+
+    def add(num1, num2):
+        try:
+            return num1 + num2
+        except TypeError as e:
+            raise exceptions.InvalidParams(str(e))
+
 
 if __name__ == '__main__':
     app.run()
