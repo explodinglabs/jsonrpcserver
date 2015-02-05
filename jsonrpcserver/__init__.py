cf2ec91d476cec7cdb13f3f474555f4ad9e6292e
Beau Barker <beauinmelbourne@gmail.com>
2015-02-05 15:01:49 +1100

    Blueprint error handling improved.
    When we take over the app_errorhandlers, previously this was done in
    __init__.py, but now we do it in a @bp.record_once decorated function. This
    keeps the code inside the blueprint file.
    
    Also, only take over errors like 404, if the Accept header is asking for json
    or json-rpc. This way we return a HTML page for regular 404s.

--- jsonrpcserver/__init__.py
+++ jsonrpcserver/__init__.py
@@ -12,14 +12,3 @@ bp = Blueprint('bp', __name__)
 from jsonrpcserver.blueprint import client_error, server_error
 from jsonrpcserver.dispatcher import dispatch
 from jsonrpcserver import exceptions, status
-
-# Override Flask's internal error handlers, to ensure we always return JSON-RPC
-for code in default_exceptions.keys():
-
-    # Client errors (4xx) should respond with "Invalid request"
-    if status.is_http_client_error(code):
-        bp.app_errorhandler(code)(client_error)
-
-    # Everything else should respond with "Server error"
-    else:
-        bp.app_errorhandler(code)(server_error)
