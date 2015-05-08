6c4827f78c88b39a8324215a48196fe69413e215
Beau Barker <beauinmelbourne@gmail.com>
2015-05-09 09:59:00 +1000

    Bumped version to 2.0.1 and updated history.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with codecs_open('HISTORY.rst', 'r', 'utf-8') as f:
 
 setup(
     name='jsonrpcserver',
-    version='2.0.0',
+    version='2.0.1',
     description='JSON-RPC server library.',
     long_description=readme + '\n\n' + history,
     author='Beau Barker',
