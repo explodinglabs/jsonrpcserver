616e5149b9cce458468251e12f4569f5ddcc25a1
Beau Barker <beauinmelbourne@gmail.com>
2015-10-09 10:35:24 +1100

    Remove numbers from HTTP status code definitions
    Resolves #36

--- test/unit/test_status.py
+++ test/unit/test_status.py
@@ -9,8 +9,8 @@ from jsonrpcserver import status
 class TestStatus(TestCase):
 
     def test_is_http_client_error(self):
-        self.assertEqual(True, status.is_http_client_error(status.HTTP_400_BAD_REQUEST))
-        self.assertEqual(False, status.is_http_client_error(status.HTTP_500_INTERNAL_ERROR))
+        self.assertEqual(True, status.is_http_client_error(status.HTTP_BAD_REQUEST))
+        self.assertEqual(False, status.is_http_client_error(status.HTTP_INTERNAL_ERROR))
 
 if __name__ == '__main__':
     main()
