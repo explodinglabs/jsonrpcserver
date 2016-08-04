a4576310ebe41c76573dc9a8ab5d929bb8c9492d
Beau Barker <beauinmelbourne@gmail.com>
2016-08-04 16:36:53 +1000

    Clean up and add some Method class tests

--- test/unit/test_methods.py
+++ test/unit/test_methods.py
@@ -10,82 +10,87 @@ from jsonrpcserver.exceptions import MethodNotFound
 
 class TestAdd(TestCase):
 
+    def test_non_callable(self):
+        m = Methods()
+        with self.assertRaises(TypeError):
+            m.add(None, 'ping')
+
     def test_function(self):
         def go(): pass # pylint: disable=multiple-statements
-        methods = Methods()
-        methods.add(go)
-        self.assertIs(go, methods['go'])
+        m = Methods()
+        m.add(go)
+        self.assertIs(go, m['go'])
 
     def test_function_custom_name(self):
         def go(): pass # pylint:disable=multiple-statements
-        methods = Methods()
-        methods.add(go, 'go_now')
-        self.assertIs(go, methods['go_now'])
+        m = Methods()
+        m.add(go, 'go_now')
+        self.assertIs(go, m['go_now'])
 
     def test_lambda_no_name(self):
         add = lambda x, y: x + y
-        methods = Methods()
-        methods.add(add) # Lambda's __name__ will be '<lambda>'!
+        m = Methods()
+        m.add(add) # Lambda's __name__ will be '<lambda>'!
         with self.assertRaises(KeyError):
-            methods['add']
+            m['add']
 
     def test_lambda_renamed(self):
         add = lambda x, y: x + y
         add.__name__ = 'add'
-        methods = Methods()
-        methods.add(add)
-        self.assertIs(add, methods['add'])
+        m = Methods()
+        m.add(add)
+        self.assertIs(add, m['add'])
 
     def test_lambda_custom_name(self):
         add = lambda x, y: x + y
-        methods = Methods()
-        methods.add(add, 'add')
-        self.assertIs(add, methods['add'])
+        m = Methods()
+        m.add(add, 'add')
+        self.assertIs(add, m['add'])
 
     def test_partial_no_name(self):
         six = partial(lambda x: x + 1, 5)
-        methods = Methods()
+        m = Methods()
         with self.assertRaises(AttributeError):
-            methods.add(six) # Partial has no __name__ !
+            m.add(six) # Partial has no __name__ !
 
     def test_partial_renamed(self):
         six = partial(lambda x: x + 1, 5)
         six.__name__ = 'six'
-        methods = Methods()
-        methods.add(six)
-        self.assertIs(six, methods['six'])
+        m = Methods()
+        m.add(six)
+        self.assertIs(six, m['six'])
 
     def test_partial_custom_name(self):
         six = partial(lambda x: x + 1, 5)
-        methods = Methods()
-        methods.add(six, 'six')
-        self.assertIs(six, methods['six'])
+        m = Methods()
+        m.add(six, 'six')
+        self.assertIs(six, m['six'])
 
     def test_static_method(self):
         class FooClass(object):
             @staticmethod
             def Foo():
                 return 'bar'
-        methods = Methods()
-        methods.add(FooClass.Foo)
-        self.assertIs(FooClass.Foo, methods['Foo'])
+        m = Methods()
+        m.add(FooClass.Foo)
+        self.assertIs(FooClass.Foo, m['Foo'])
 
     def test_static_method_custom_name(self):
         class FooClass(object):
             @staticmethod
             def Foo():
                 return 'bar'
-        methods = Methods()
-        methods.add(FooClass.Foo, 'custom')
-        self.assertIs(FooClass.Foo, methods['custom'])
+        m = Methods()
+        m.add(FooClass.Foo, 'custom')
+        self.assertIs(FooClass.Foo, m['custom'])
 
     def test_instance_method(self):
         class FooClass(object):
             def Foo(self): # pylint: disable=no-self-use
                 return 'bar'
-        methods = Methods()
-        methods.add(FooClass().Foo)
-        self.assertEqual('bar', methods['Foo'].__call__())
+        m = Methods()
+        m.add(FooClass().Foo)
+        self.assertEqual('bar', m['Foo'].__call__())
 
     def test_instance_method_custom_name(self):
         class Foo(object):
@@ -95,30 +100,48 @@ class TestAdd(TestCase):
                 return self.name
         obj1 = Foo('a')
         obj2 = Foo('b')
-        methods = Methods()
-        methods.add(obj1.get_name, 'custom1')
-        methods.add(obj2.get_name, 'custom2')
+        m = Methods()
+        m.add(obj1.get_name, 'custom1')
+        m.add(obj2.get_name, 'custom2')
         # Can't use assertIs, so check the outcome is as expected
-        self.assertEqual('a', methods['custom1'].__call__())
-        self.assertEqual('b', methods['custom2'].__call__())
+        self.assertEqual('a', m['custom1'].__call__())
+        self.assertEqual('b', m['custom2'].__call__())
+
+
+class TestAddDictLike(TestCase):
+
+    def test_function(self):
+        m = Methods()
+        def ping():
+            return 'pong'
+        m['ping'] = ping
+
+    def test_lambda(self):
+        m = Methods()
+        m['ping'] = lambda: 'pong'
+
+    def test_non_callable(self):
+        m = Methods()
+        with self.assertRaises(TypeError):
+            m['ping'] = 1
 
 
 class TestDecorator(TestCase):
 
     def test_function(self):
-        methods = Methods()
-        @methods.add
+        m = Methods()
+        @m.add
         def go(): pass # pylint:disable=multiple-statements
-        self.assertIs(go, methods['go'])
+        self.assertIs(go, m['go'])
 
     def test_static_method(self):
-        methods = Methods()
+        m = Methods()
         class FooClass(object):
             @staticmethod
-            @methods.add
+            @m.add
             def Foo():
                 return 'bar'
-        self.assertIs(FooClass.Foo, methods['Foo'])
+        self.assertIs(FooClass.Foo, m['Foo'])
 
 
 if __name__ == '__main__':
