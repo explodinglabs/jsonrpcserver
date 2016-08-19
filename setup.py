596e9b8a24ec383dfb9d59ab1de21e2ed9a9cc81
Beau Barker <beauinmelbourne@gmail.com>
2016-08-19 11:55:11 +1000

    Bump version to 3.3.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with codecs_open('HISTORY.rst', 'r', 'utf-8') as f:
 
 setup(
     name='jsonrpcserver',
-    version='3.3.1',
+    version='3.3.2',
     description='Process JSON-RPC requests',
     long_description=readme + '\n\n' + history,
     author='Beau Barker',
