bc79f9df9226e9a18d2396751589aaf506d3ab20
Beau Barker <beauinmelbourne@gmail.com>
2016-09-22 16:45:11 +1000

    Adjust socketio server example

--- examples/socketio_server.py
+++ examples/socketio_server.py
@@ -2,13 +2,12 @@ from flask import Flask
 from flask_socketio import SocketIO
 from jsonrpcserver import Methods, dispatch
 
-app = Flask(__name__)
-
 methods = Methods()
 @methods.add
 def ping():
     return 'pong'
 
+app = Flask(__name__)
 socketio = SocketIO(app)
 @socketio.on('message')
 def handle_message(request):
