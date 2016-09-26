196369e7dd50a65631308719ad470a4feb1356d8
Beau Barker <beauinmelbourne@gmail.com>
2016-09-27 01:35:10 +1000

    Move the basic http server into Methods class
    Previously it was a parent of Methods

--- jsonrpcserver/methods.py
+++ jsonrpcserver/methods.py
@@ -10,16 +10,28 @@ like ``[eat, drink]``, a dictionary, or a ``Methods`` object::
 
     dispatch(methods, ...)
 """
+import logging
 try:
     # Python 2
     from collections import MutableMapping
 except ImportError:
     # Python 3
     from collections.abc import MutableMapping
-from .http_server import MethodsServer
+try:
+    # Python 2
+    from BaseHTTPServer import HTTPServer
+    from BaseHTTPServer import BaseHTTPRequestHandler
+except ImportError:
+    # Python 3
+    from http.server import BaseHTTPRequestHandler, HTTPServer
+
+from .log import log_
+from .dispatcher import dispatch
+
+_LOGGER = logging.getLogger(__name__)
 
 
-class Methods(MutableMapping, MethodsServer): #pylint:disable=too-many-ancestors
+class Methods(MutableMapping): #pylint:disable=too-many-ancestors
     """Holds a list of methods
     ... versionchanged:: 3.3
         Subclass MutableMapping instead of dict.
@@ -77,3 +89,26 @@ class Methods(MutableMapping, MethodsServer): #pylint:disable=too-many-ancestors
             Use add instead.
         """
         return self.add(*args, **kwargs)
+
+    def serve_forever(self, name='', port=5000):
+        """Serve the methods with a basic http server"""
+
+        class RequestHandler(BaseHTTPRequestHandler):
+            """Handles HTTP requests"""
+            def do_POST(self): #pylint:disable=invalid-name
+                """Takes a HTTP POST request, processes it and returns the result"""
+                # Process request
+                request = self.rfile.read(
+                    int(self.headers['Content-Length'])).decode()
+                response = dispatch(self.server.methods, request)
+                # Return response
+                self.send_response(response.http_status)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+                self.wfile.write(str(response).encode())
+
+        httpd = HTTPServer((name, port), RequestHandler)
+        # Let the request handler know which methods to dispatch to
+        httpd.methods = self
+        log_(_LOGGER, 'info', ' * Listening on port %s', port)
+        httpd.serve_forever()
