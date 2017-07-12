a93aaea862cf33f063cc44cead9f7b3c1001df8b
Beau Barker <beauinmelbourne@gmail.com>
2017-07-12 17:40:01 +1000

    Add async test

---
+++ test/test_async_request.py
@@ -0,0 +1,34 @@
+import inspect, asyncio
+from unittest import TestCase, main
+
+from jsonrpcserver.async_request import AsyncRequest
+from jsonrpcserver.aio import methods
+
+
+def async_test(f):
+    def wrapper(*args, **kwargs):
+        if inspect.iscoroutinefunction(f):
+            future = f(*args, **kwargs)
+        else:
+            coroutine = asyncio.coroutine(f)
+            future = coroutine(*args, **kwargs)
+        asyncio.get_event_loop().run_until_complete(future)
+    return wrapper
+
+
+class MyMethods():
+    async def foo(self):
+        return 'bar'
+
+methods.add(MyMethods().foo)
+
+class TestCall(TestCase):
+    @async_test
+    async def test(self):
+        req = AsyncRequest({'jsonrpc': '2.0', 'method': 'foo', 'id': 1})
+        response = await req.call(methods)
+        self.assertEqual('bar', response['result'])
+
+
+if __name__ == '__main__':
+    main()
