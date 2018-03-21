616c0b0722d7b81cad1b4f8d2cfaec24dcfcbf71
Beau Barker <beauinmelbourne@gmail.com>
2018-03-21 15:04:21 +1100

    Set readme content type to markdown

--- setup.py
+++ setup.py
@@ -4,23 +4,20 @@ from setuptools import setup
 
 with codecs_open('README.md', 'r', 'utf-8') as f:
     README = f.read()
-
 with codecs_open('HISTORY.md', 'r', 'utf-8') as f:
     HISTORY = f.read()
 
 setup(
-    name='jsonrpcserver',
-    version='3.5.3',
-    description='Process JSON-RPC requests',
-    long_description=README + '\n\n' + HISTORY,
     author='Beau Barker',
     author_email='beauinmelbourne@gmail.com',
-    url='https://github.com/bcb/jsonrpcserver',
-    license='MIT',
-    packages=['jsonrpcserver'],
-    package_data={'jsonrpcserver': ['request-schema.json']},
-    include_package_data=True,
-    install_requires=['jsonschema', 'six', 'funcsigs'],
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+    description='Process JSON-RPC requests',
     extras_require={
         'tox': ['tox'],
         'examples': [
@@ -34,11 +31,14 @@ setup(
             'werkzeug',
         ]
     },
-    classifiers=[
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
-    ],
+    include_package_data=True,
+    install_requires=['jsonschema', 'six', 'funcsigs'],
+    license='MIT',
+    long_description=README + '\n\n' + HISTORY,
+    long_description_content_type='text/markdown',
+    name='jsonrpcserver',
+    package_data={'jsonrpcserver': ['request-schema.json']},
+    packages=['jsonrpcserver'],
+    url='https://github.com/bcb/jsonrpcserver',
+    version='3.5.3'
 )
