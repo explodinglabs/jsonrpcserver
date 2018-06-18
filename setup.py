273ab006b571d5377fe8fa7f5df6cfde1aaddc52
Beau Barker <beauinmelbourne@gmail.com>
2018-06-18 17:18:08 +1000

    Rename HISTORY.md to CHANGELOG.md

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ from setuptools import setup
 
 with codecs_open('README.md', 'r', 'utf-8') as f:
     README = f.read()
-with codecs_open('HISTORY.md', 'r', 'utf-8') as f:
-    HISTORY = f.read()
 
 setup(
     author='Beau Barker',
@@ -34,7 +32,7 @@ setup(
     include_package_data=True,
     install_requires=['jsonschema', 'six', 'funcsigs'],
     license='MIT',
-    long_description=README + '\n\n' + HISTORY,
+    long_description=README,
     long_description_content_type='text/markdown',
     name='jsonrpcserver',
     package_data={'jsonrpcserver': ['request-schema.json']},
