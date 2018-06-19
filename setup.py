2255a4c6cd6f4afa28e55d2920a9e6b61ceaf916
Beau Barker <beauinmelbourne@gmail.com>
2018-06-19 22:33:50 +1000

    Limit versions of dependencies in setup.py
    ".. it’s best practice to indicate any known lower or upper bounds"
    https://packaging.python.org/discussions/install-requires-vs-requirements/

--- setup.py
+++ setup.py
@@ -2,41 +2,41 @@
 from codecs import open as codecs_open
 from setuptools import setup
 
-with codecs_open('README.md', 'r', 'utf-8') as f:
+with codecs_open("README.md", "r", "utf-8") as f:
     README = f.read()
 
 setup(
-    author='Beau Barker',
-    author_email='beauinmelbourne@gmail.com',
+    author="Beau Barker",
+    author_email="beauinmelbourne@gmail.com",
     classifiers=[
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
     ],
-    description='Process JSON-RPC requests',
+    description="Process JSON-RPC requests",
     extras_require={
-        'tox': ['tox'],
-        'examples': [
-            'aiohttp',
-            'aiozmq',
-            'flask',
-            'flask-socketio',
-            'pyzmq',
-            'tornado',
-            'websockets',
-            'werkzeug',
-        ]
+        "tox": ["tox"],
+        "examples": [
+            "aiohttp",
+            "aiozmq",
+            "flask",
+            "flask-socketio",
+            "pyzmq",
+            "tornado",
+            "websockets",
+            "werkzeug",
+        ],
     },
     include_package_data=True,
-    install_requires=['jsonschema', 'six', 'funcsigs'],
-    license='MIT',
+    install_requires=["jsonschema>=2,<3", "six>=1,<2", "funcsigs>=1,<2"],
+    license="MIT",
     long_description=README,
-    long_description_content_type='text/markdown',
-    name='jsonrpcserver',
-    package_data={'jsonrpcserver': ['request-schema.json']},
-    packages=['jsonrpcserver'],
-    url='https://github.com/bcb/jsonrpcserver',
-    version='3.5.5'
+    long_description_content_type="text/markdown",
+    name="jsonrpcserver",
+    package_data={"jsonrpcserver": ["request-schema.json"]},
+    packages=["jsonrpcserver"],
+    url="https://github.com/bcb/jsonrpcserver",
+    version="3.5.5",
 )
