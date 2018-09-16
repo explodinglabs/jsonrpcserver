54ed561f78207f8f50d41bb4e8a78b21efca6276
Beau Barker <beauinmelbourne@gmail.com>
2018-09-16 11:24:19 +1000

    Begin work on the request module

--- jsonrpcserver/request.py
+++ jsonrpcserver/request.py
@@ -20,8 +20,10 @@ from .response import (
     Response,
 )
 
+UNSPECIFIED = object()
 
-def convert_camel_case(name: str) -> str:
+
+def convert_camel_case_string(name: str) -> str:
     """Convert camel case string to snake case"""
     string = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", name)
     return re.sub("([a-z0-9])([A-Z])", r"\1_\2", string).lower()
@@ -33,9 +35,9 @@ def convert_camel_case_keys(original_dict: Request) -> Request:
     for key, val in original_dict.items():
         if isinstance(val, dict):
             # Recurse
-            new_dict[convert_camel_case(key)] = convert_camel_case_keys(val)
+            new_dict[convert_camel_case_string(key)] = convert_camel_case_keys(val)
         else:
-            new_dict[convert_camel_case(key)] = val
+            new_dict[convert_camel_case_string(key)] = val
     return new_dict
 
 
@@ -45,74 +47,54 @@ def validate_arguments_against_signature(
     """
     Check if the request's arguments match a function's signature.
 
-    Raises InvalidParams exception if arguments cannot be passed to a
-    function.
+    Raises InvalidParams exception if arguments cannot be passed to a function.
 
-    :param func: The function to check.
-    :param args: Positional arguments.
-    :param kwargs: Keyword arguments.
-    :raises InvalidParams: If the arguments cannot be passed to the function.
+    Args:
+        func: The function to check.
+        args: Positional arguments.
+        kwargs: Keyword arguments.
+
+    Raises:
+        TypeError: If the arguments cannot be passed to the function.
     """
-    try:
-        signature(func).bind(*(args or []), **(kwargs or {}))
-    except TypeError as exc:
-        raise InvalidParams(str(exc))
-
-
-def get_method(methods: Methods, name: str) -> Callable:
-    """
-    Find a method in a list (or dictionary).
-
-    :param methods: List or dictionary of named functions.
-    :param name: Name of the method to find.
-    :raises MethodNotFound: If the method wasn't in the list.
-    :returns: The method from the list.
-    """
-    # If it's a Mapping (dict-like), search for the key
-    if isinstance(methods, Mapping):
-        try:
-            return methods[name]
-        except KeyError:
-            raise MethodNotFound(name)
-    # Otherwise it must be a Sequence, search the __name__ attributes of
-    # its items
-    elif isinstance(methods, Sequence):
-        try:
-            return next(m for m in methods if m.__name__ == name)
-        except StopIteration:
-            raise MethodNotFound(name)
+    signature(func).bind(*(args or []), **(kwargs or {}))
 
 
 def get_arguments(
-    params: Union[List, Dict], context: Optional[Dict] = None
+    params: Union[List, Dict], context: Union[Dict, object] = NOTSPECIFIED
 ) -> Tuple[Optional[List], Optional[Dict]]:
     """
-    Get the positional and keyword arguments of a request.
+    Get the positional and keyword arguments from a request.
 
-    Takes the 'params' part of a JSON-RPC request and converts it to either
-    positional or keyword arguments usable in a Python function call. Note that
-    a JSON-RPC request can have positional or keyword arguments, but not both.
-    See http://www.jsonrpc.org/specification#parameter_structures
+    Takes the 'params' part of a JSON-RPC request and converts it to either positional
+    or keyword arguments usable in a Python function call. Note that a JSON-RPC request
+    can have positional or named arguments, but not both. See
+    http://www.jsonrpc.org/specification#parameter_structures
 
-    :param params: The 'params' part of the JSON-RPC request (should be a
-        list or dict). The 'params' value can be a JSON array (Python list),
-        object (Python dict), or None.
-    :param context: Optionally include some context data, which will be included
-        in the keyword arguments passed to the method.
-    :raises InvalidParams: If 'params' was present but was not a list or
-        dict.
-    :returns: A two-tuple containing the positionals (in a list, or None) and
-        keywords (in a dict, or None) extracted from the 'params' part of the
-        request.
+    Args:
+        params: The 'params' part of the JSON-RPC request (should be a list or dict).
+            The 'params' value can be a JSON array (Python list), object (Python dict),
+            or None.
+        context: Optionally include some context data, which will be included in the
+            keyword arguments passed to the method.
+
+    Returns:
+        A two-tuple containing the positional (in a list, or None) and named (in a dict,
+        or None) arguments, extracted from the 'params' part of the request.
+
+    Raises:
+        InvalidParams: If 'params' was present but was not a list or dict.
+        AssertionError: If both positional and names arguments specified, which is not
+            allowed in JSON-RPC.
     """
-    positionals = keywords = None
+    positionals = nameds = None
     if params:
         # Params is a list. Taken as positional arguments.
         if isinstance(params, list):
             positionals = params
         # Params is a dict. Taken as keyword arguments.
         elif isinstance(params, dict):
-            keywords = params
+            nameds = params
         # Any other type is invalid. (This should never happen if the request
         # has passed the schema validation.)
         else:
@@ -122,14 +104,13 @@ def get_arguments(
     # Can't have both positional and keyword arguments. It's impossible in json
     # anyway; the params arg can only be a json array (list) or object (dict)
     assert not (
-        positionals and keywords
+        positionals and nameds
     ), "Cannot have both positional and keyword arguments in JSON-RPC."
     # If context data was passed, include it as a keyword argument.
-    # TODO: Remove this predicate; None is a valid argument.
-    if context is not None:
-        keywords = {} if not keywords else keywords
-        keywords["context"] = context
-    return (positionals, keywords)
+    if context is not UNSPECIFIED:
+        nameds = {} if not nameds else nameds
+        nameds["context"] = context
+    return (positionals, nameds)
 
 
 class Request:
@@ -160,16 +141,15 @@ class Request:
         self.request_id = request.get("id")
 
         if convert_camel_case:
-            self.method_name = convert_camel_case(self.method_name)
+            self.method_name = convert_camel_case_string(self.method_name)
             if self.kwargs:
                 self.kwargs = convert_camel_case_keys(self.kwargs)
 
     @property
     def is_notification(self) -> bool:
         """
-        Returns True if the request is a JSON-RPC Notification (ie. No id attribute is
-        included). False if it's a request.
+        Returns:
+            True if the request is a JSON-RPC Notification (ie. No id attribute is
+            included). False if it doesn't, meaning it's a JSON-RPC "Request".
         """
         return request.get("id") is None
-
-
--- tests/test_request.py
+++ tests/test_request.py
@@ -9,25 +9,18 @@ from jsonrpcserver.request import Request
 from jsonrpcserver.response import ErrorResponse, NotificationResponse, RequestResponse
 
 
-# Some dummy functions to use for testing
-def foo():
-    return "bar"
+class TestConvertCamelCase(TestCase):
+    def test(self):
+        self.assertEqual("foo_bar", convert_camel_case("fooBar"))
 
 
-class Foo:
-    def foo(self, one, two):
-        return "bar"
-
-
-FOO, BAR = (object(), object())
-
-
-def setUpModule():
-    config.debug = True
-
-
-def tearDownModule():
-    config.debug = False
+class TestConvertCamelCaseKeys(TestCase):
+    def test(self):
+        dictionary = {"fooKey": 1, "aDict": {"fooKey": 1, "barKey": 2}}
+        self.assertEqual(
+            {"foo_key": 1, "a_dict": {"foo_key": 1, "bar_key": 2}},
+            convert_camel_case_keys(dictionary),
+        )
 
 
 class TestInit(TestCase):
@@ -249,3 +242,69 @@ class TestRequestProcessNotifications(TestCase):
         req = Request({"jsonrpc": "2.0", "method": "foo"}).call([foo])
         self.assertEqual(status.HTTP_OK, req.http_status)
         NotificationResponse.http_status = status.HTTP_NO_CONTENT
+
+
+class TestValidateArgumentsAgainstSignature(TestCase):
+    """Keep it simple here. No need to test signature.bind."""
+
+    @staticmethod
+    def test_no_arguments():
+        validate_arguments_against_signature(lambda: None, None, None)
+
+    def test_no_arguments_too_many_positionals(self):
+        with self.assertRaises(InvalidParams):
+            validate_arguments_against_signature(lambda: None, ["foo"], None)
+
+    @staticmethod
+    def test_positionals():
+        validate_arguments_against_signature(lambda x: None, [1], None)
+
+    def test_positionals_not_passed(self):
+        with self.assertRaises(InvalidParams):
+            validate_arguments_against_signature(lambda x: None, None, {"foo": "bar"})
+
+    @staticmethod
+    def test_keywords():
+        validate_arguments_against_signature(
+            lambda **kwargs: None, None, {"foo": "bar"}
+        )
+
+    @staticmethod
+    def test_object_method():
+        validate_arguments_against_signature(FooClass().foo, ["one", "two"], None)
+
+
+class TestGetArguments(TestCase):
+    def test_none(self):
+        self.assertEqual((None, None), get_arguments(None))
+
+    def test_positional(self):
+        self.assertEqual(([2, 3], None), get_arguments([2, 3]))
+
+    def test_keyword(self):
+        self.assertEqual((None, {"foo": "bar"}), get_arguments({"foo": "bar"}))
+
+    #    def test_invalid_none(self):
+    #        with self.assertRaises(InvalidParams):
+    #            get_arguments(None)
+
+    def test_invalid_numeric(self):
+        with self.assertRaises(InvalidParams):
+            get_arguments(5)
+
+    def test_invalid_string(self):
+        with self.assertRaises(InvalidParams):
+            get_arguments("str")
+
+    # With the "context" argument
+    def test_no_arguments_with_context(self):
+        args = get_arguments(None, context="foo")
+        self.assertEqual((None, {"context": "foo"}), args)
+
+    def test_positional_with_context(self):
+        args = get_arguments(["foo"], context="bar")
+        self.assertEqual((["foo"], {"context": "bar"}), args)
+
+    def test_keyword_with_context(self):
+        args = get_arguments({"foo": "bar"}, context="baz")
+        self.assertEqual((None, {"foo": "bar", "context": "baz"}), args)
