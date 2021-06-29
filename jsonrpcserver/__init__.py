021ba2b31915c27f015bee1a817cc46d6bddedee
Beau Barker <beauinmelbourne@gmail.com>
2021-06-30 08:12:51 +1000

    Remove async from type checking for now

--- jsonrpcserver/__init__.py
+++ jsonrpcserver/__init__.py
@@ -1,4 +1,4 @@
-from .async_dispatcher import dispatch as async_dispatch
+# from .async_dispatcher import dispatch as async_dispatch
 from .dispatcher import dispatch
 from .methods import add as method
 from .result import Result, Success, Error, InvalidParams
