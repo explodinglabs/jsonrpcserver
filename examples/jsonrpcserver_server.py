5a979c0ac389ecd3cfee0a4a29b20ac004494d00
Beau Barker <beauinmelbourne@gmail.com>
2021-07-30 21:28:25 +1000

    Update examples/jsonrpcserver_server.py to v5

--- examples/jsonrpcserver_server.py
+++ examples/jsonrpcserver_server.py
@@ -1,10 +1,10 @@
 """Using jsonrpcserver's built-in serve() function"""
-from jsonrpcserver import method, serve
+from jsonrpcserver import Success, method, serve
 
 
 @method
 def ping():
-    return "pong"
+    return Success("pong")
 
 
 if __name__ == "__main__":
