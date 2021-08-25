661dbd365b4fef2348f6db48a536515085f028cd
Beau Barker <beau@explodinglabs.com>
2021-08-25 10:33:44 +1000

    Replace compose function with a better one

--- jsonrpcserver/utils.py
+++ jsonrpcserver/utils.py
@@ -5,8 +5,11 @@ from typing import Any, Callable, List
 identity = lambda x: x
 
 
-def compose(*funcs: Callable[..., Any]) -> Callable[..., Any]:
-    return lambda x: reduce(lambda acc, f: f(acc), reversed(funcs), x)
+def compose(*fs: Callable[..., Any]) -> Callable[..., Any]:
+    def compose2(f: Callable[..., Any], g: Callable[..., Any]) -> Callable[..., Any]:
+        return lambda *a, **kw: f(g(*a, **kw))
+
+    return reduce(compose2, fs)
 
 
 def make_list(x: Any) -> List[Any]:
